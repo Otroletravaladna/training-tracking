4190c594be927333db2dd41f3f3db1e7bef009ff
Otroletravaladna <crnana98@gmail.com>
2022-12-06 15:59:23 -0300

    Fix local storage iteration bug

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const submit = document.querySelector("#submit");
 const form = document.querySelector("form");
 const tableBody = document.querySelector("tbody");
 
-let expenses = [];
+let expenses = [
+    // {purchase: 'Boleto El Tio-Temple', place: 'Terminal', date: '2022-12-03', price: '500', payment: 'cash'}
+];
 
 function Purchase() {
     this.purchase = document.querySelector("#purchase").value;
@@ -29,34 +31,37 @@ Purchase.prototype.createRow = function() {
 }
 
 window.addEventListener("DOMContentLoaded", (e) =>{
-    for(const item of JSON.parse(newObject)){
-        let newRow = document.createElement("tr");
-        let dataPur = document.createElement("td");
-        let dataPlace = document.createElement("td");
-        let dataDate = document.createElement("td");
-        let dataEval = document.createElement("td");
-        let dataPrice = document.createElement("td");
-        tableBody.appendChild(newRow);
-        newRow.append(dataPur, dataPlace, dataDate, dataEval, dataPrice);
-        dataPur.textContent = item.purchase;
-        dataPlace.textContent = item.place;
-        dataDate.textContent = item.date;
-        dataEval.textContent = "";
-        dataPrice.textContent = item.price;
+    e.preventDefault();
+    if (archive != null) {
+        for(const item of archive){
+            let newRow = document.createElement("tr");
+            let dataPur = document.createElement("td");
+            let dataPlace = document.createElement("td");
+            let dataDate = document.createElement("td");
+            let dataEval = document.createElement("td");
+            let dataPrice = document.createElement("td");
+            tableBody.appendChild(newRow);
+            newRow.append(dataPur, dataPlace, dataDate, dataEval, dataPrice);
+            dataPur.textContent = item.purchase;
+            dataPlace.textContent = item.place;
+            dataDate.textContent = item.date;
+            dataEval.textContent = "";
+            dataPrice.textContent = item.price;
+        }
     }
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let newExpense = new Purchase();
-        expenses.push(newExpense);
-        newExpense.createRow();
-        localStorage.setItem("ExpensesList", JSON.stringify(expenses));
-    })
+})
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newExpense = new Purchase();
+    expenses.push(newExpense);
+    newExpense.createRow();
+
+    localStorage.setItem("ExpensesList", JSON.stringify(expenses));
 })
 
 let newObject = window.localStorage.getItem("ExpensesList");
-console.log(JSON.parse(newObject));
-
-
-
+let archive = (JSON.parse(newObject));
+console.log(archive);
 
 
