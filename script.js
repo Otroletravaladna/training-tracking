88e7594c73c16cd3d62e921f4be08c37f7b2da76
Otroletravaladna <crnana98@gmail.com>
2023-01-18 03:00:33 -0300

    Change object constructors to classes

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const submit = document.querySelector("#submit");
 const form = document.querySelector("form");
 const tableBody = document.querySelector("tbody");
 
-function Purchase() {
-    this.purchase = document.querySelector("#purchase").value;
-    this.place = document.querySelector("#purchase-place").value;
-    this.date = document.querySelector("#purchase-date").value;
-    this.evaluation = document.querySelector("#eval").value;
-    this.payment = document.querySelector("#payment").value;
-    this.price = document.querySelector("#purchase-price").value;
+class Purchase {
+    constructor() {
+        this.purchase = document.querySelector("#purchase").value;
+        this.place = document.querySelector("#purchase-place").value;
+        this.date = document.querySelector("#purchase-date").value;
+        this.evaluation = document.querySelector("#eval").value;
+        this.payment = document.querySelector("#payment").value;
+        this.price = document.querySelector("#purchase-price").value;
+    }
 
-    const createRow = (() => {
+    createRow = () => {
         let newRow = document.createElement("tr");
         let dataPur = document.createElement("td");
         let dataPlace = document.createElement("td");
@@ -26,30 +28,47 @@ function Purchase() {
         dataEval.textContent = this.evaluation;
         dataPrice.textContent = this.price;
         dataPay.textContent = this.payment;
-    })();
-}
+        console.log(this);
+    };
+};
 
+// function Purchase() {
+//     this.purchase = document.querySelector("#purchase").value;
+//     this.place = document.querySelector("#purchase-place").value;
+//     this.date = document.querySelector("#purchase-date").value;
+//     this.evaluation = document.querySelector("#eval").value;
+//     this.payment = document.querySelector("#payment").value;
+//     this.price = document.querySelector("#purchase-price").value;
 
-// Purchase.prototype.createRow = function() {
-//     let newRow = document.createElement("tr");
-//     let dataPur = document.createElement("td");
-//     let dataPlace = document.createElement("td");
-//     let dataDate = document.createElement("td");
-//     let dataEval = document.createElement("td");
-//     let dataPay = document.createElement("td");
-//     let dataPrice = document.createElement("td");
-//     tableBody.appendChild(newRow);
-//     newRow.append(dataPur, dataPlace, dataDate, dataEval,dataPay, dataPrice);
-//     dataPur.textContent = this.purchase;
-//     dataPlace.textContent = this.place;
-//     dataDate.textContent = this.date;
-//     dataEval.textContent = this.evaluation;
-//     dataPrice.textContent = this.price;
-//     dataPay.textContent = this.payment;
+//     const createRow = (() => {
+//         let newRow = document.createElement("tr");
+//         let dataPur = document.createElement("td");
+//         let dataPlace = document.createElement("td");
+//         let dataDate = document.createElement("td");
+//         let dataEval = document.createElement("td");
+//         let dataPay = document.createElement("td");
+//         let dataPrice = document.createElement("td");
+//         tableBody.appendChild(newRow);
+//         newRow.append(dataPur, dataPlace, dataDate, dataEval,dataPay, dataPrice);
+//         dataPur.textContent = this.purchase;
+//         dataPlace.textContent = this.place;
+//         dataDate.textContent = this.date;
+//         dataEval.textContent = this.evaluation;
+//         dataPrice.textContent = this.price;
+//         dataPay.textContent = this.payment;
+//     })();
 // }
 
 window.addEventListener("DOMContentLoaded", (e) =>{
     e.preventDefault();
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let newExpense = new Purchase();
+        newExpense.createRow();
+        archive.push(newExpense);
+                
+        localStorage.setItem("ExpensesList", JSON.stringify(archive));
+    })
     if (archive != null) {
         for(const item of archive){
             let newRow = document.createElement("tr");
@@ -70,18 +89,6 @@ window.addEventListener("DOMContentLoaded", (e) =>{
         }
     }
 })
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let newExpense = new Purchase();
-    archive.push(newExpense);
-    // newExpense.createRow();
-
-    localStorage.setItem("ExpensesList", JSON.stringify(archive));
-
-  
-})
-
 
 let archive = JSON.parse(localStorage.getItem("ExpensesList")) || [];
 
